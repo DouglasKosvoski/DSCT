1044016fd435af91adb94b6db41b0a24c94b87b7
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-11-26 11:05:29 -0300

    wip admin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var databases;
+
+fs.readFile('./public/data/characters.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`Error reading file from disk: ${err}`);
+  }
+  else {
+    // parse JSON string to JSON object
+    databases = JSON.parse(data);
+    // print all databases
+    // databases.forEach(db => {
+    //   console.log(`${db.name}: ${db.type}`);
+    // });
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dark Souls Chronological Timeline' });
+  res.render('index', {
+    title: 'Dark Souls Chronological Timeline',
+    data: databases
+  });
 });
 
 module.exports = router;
